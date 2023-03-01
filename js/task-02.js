327ff158b369ac0763e2932375463778b39749c2
Illia Bidiuk <Core778@gmail.com>
2023-03-01 23:26:02 +0200

    first commit

--- js/task-02.js
+++ js/task-02.js
@@ -7,38 +7,44 @@ const ingredients = [
   'Condiments',
 ];
 
-ingredients.forEach((ingredient)=> {
+const ingredientsList = document.querySelector("#ingredients");
+const liEl = document.createElement('li');
+
+const eatEl = ingredients.forEach((ingredient, index)=> {
   
+liEl.textContent = ingredient;
+liEl.classList.add("item");
+ingredientsList.prepend(liEl  )
 })
 
-const ingredientsList = document.querySelector("#ingredients");
 
-const liEl5 = document.createElement('li');
-liEl5.textContent ='Condiments';
-liEl5.classList.add("item");
-ingredientsList.prepend(liEl5)
 
-const liEl4 = document.createElement('li');
-liEl4.textContent ='Herbs';
-liEl4.classList.add("item");
-ingredientsList.prepend(liEl4)
+// const liEl5 = document.createElement('li');
+// liEl5.textContent ='Condiments';
+// liEl5.classList.add("item");
+// ingredientsList.prepend(liEl5)
 
-const liEl3 = document.createElement('li');
-liEl3.textContent ='Tomatos';
-liEl3.classList.add("item");
-ingredientsList.prepend(liEl3)
+// const liEl4 = document.createElement('li');
+// liEl4.textContent ='Herbs';
+// liEl4.classList.add("item");
+// ingredientsList.prepend(liEl4)
 
-const liEl2 = document.createElement('li');
-liEl2.textContent ='Garlic';
-liEl2.classList.add("item");
-ingredientsList.prepend(liEl2)
+// const liEl3 = document.createElement('li');
+// liEl3.textContent ='Tomatos';
+// liEl3.classList.add("item");
+// ingredientsList.prepend(liEl3)
 
-const liEl1 = document.createElement('li');
-liEl1.textContent = 'Mushrooms';
-liEl1.classList.add("item");
-ingredientsList.prepend(liEl1)
+// const liEl2 = document.createElement('li');
+// liEl2.textContent ='Garlic';
+// liEl2.classList.add("item");
+// ingredientsList.prepend(liEl2)
 
-const liEl = document.createElement('li');
-liEl.textContent = 'Potatoes';
-liEl.classList.add("item");
-ingredientsList.prepend(liEl)
+// const liEl1 = document.createElement('li');
+// liEl1.textContent = 'Mushrooms';
+// liEl1.classList.add("item");
+// ingredientsList.prepend(liEl1)
+
+// const liEl = document.createElement('li');
+// liEl.textContent = 'Potatoes';
+// liEl.classList.add("item");
+// ingredientsList.prepend(liEl)
--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,9 @@
 const validationInput = document.querySelector('#validation-input');
 
 validationInput.addEventListener('blur', (evt) => {
-    evt.target.classList.toggle('valid');
-    if (evt.target.value.length === evt.target.dataset.length) {
-       evt.target.classList.toggle('invalid');
+    evt.target.classList.add('invalid');
+    if (evt.target.value.length === Number(evt.target.dataset.length)) {
+        evt.target.classList.replace("invalid", "valid");
     }
      
 });
